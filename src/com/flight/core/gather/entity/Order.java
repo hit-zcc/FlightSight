def298d6b64cf9beb286fbac9ac887db16323d61
ZCC <ZCC@DESKTOP-0A9C095>
2017-02-16 23:40:43 +0800

    mongo配置增加

---
+++ src/com/flight/core/gather/entity/Order.java
@@ -0,0 +1,9 @@
+package com.flight.core.gather.entity;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.annotation.Id;
+@Document
+public class Order {
+  @Id
+  private String id; 
+}
